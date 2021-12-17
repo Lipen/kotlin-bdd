38df986a5b6d661e2bf282d0bba617e08c0df947
Konstantin Chukharev <lipen00@gmail.com>
2021-12-18 02:36:20 +0300

    Improve internals of PHP example

--- examples/src/main/kotlin/com/github/lipen/bdd/PHP.kt
+++ examples/src/main/kotlin/com/github/lipen/bdd/PHP.kt
@@ -50,11 +50,16 @@ fun php(pigeons: Int, holes: Int = pigeons - 1) {
         else -> 1 shl 20
     })
     var f = bdd.one
-    var g: Ref? = null
     var totaltime = 0.0
 
     val useGC = false
+    val useCojoinTree = false
     val useProj = true
+    val useRelProd = true
+
+    if (useRelProd && !useProj) {
+        error("In order to use relProd, you must use Proj")
+    }
 
     fun v(i: Int, j: Int) = (i - 1) * holes + j
 
@@ -65,33 +70,24 @@ fun php(pigeons: Int, holes: Int = pigeons - 1) {
     file.sink().buffer().use {
         with(it) {
             writeUtf8("step name BDD.size f.size steptime totaltime hits misses\n")
-            // writeUtf8("0 init ${bdd.realSize()} ${bdd.descendants(1).size} 0.0 0.0 0 0\n")
 
             var count = 0
 
-            fun step(name: String, block: () -> Unit): Double {
+            fun step(name: String, allowGC: Boolean = false, block: () -> Unit): Double {
                 count++
                 val steptime = measureTime {
                     block()
-                    if (useGC) {
+                    if (allowGC) {
                         if (count % 50 == 0) {
-                            if (g != null) {
-                                bdd.collectGarbage(listOf(f, g!!))
-                            } else {
-                                bdd.collectGarbage(listOf(f))
-                            }
+                            bdd.collectGarbage(listOf(f))
                         }
                     }
                 }
                 val fsize = bdd.descendants(f).size
-                val gsize = if (g != null) {
-                    bdd.descendants(g!!).size
-                } else {
-                    0
-                }
                 println(
-                    ("Step #$count ($name) in %.3fms, bdd.size=${bdd.size}, bdd.realSize=${bdd.realSize}," +
-                        " f=$f (v=${bdd.getTriplet(f)?.v}), f.size=$fsize, g.fize=$gsize," +
+                    ("Step #$count ($name) in %.3fms," +
+                        " bdd.size=${bdd.size}, bdd.realSize=${bdd.realSize}," +
+                        " f=$f (v=${bdd.getTriplet(f)?.v}), f.size=$fsize," +
                         " hits=${bdd.cacheHits}, misses=${bdd.cacheMisses}}")
                         .format(steptime.milliseconds)
                 )
@@ -105,7 +101,7 @@ fun php(pigeons: Int, holes: Int = pigeons - 1) {
             }
 
             for (clause in generatePhpClausesA(pigeons, holes)) {
-                step("JoinA") {
+                step("JoinA", useGC) {
                     val c = bdd.clause(clause)
                     check(!bdd.isZero(f))
                     f = bdd.applyAnd(f, c)
@@ -113,44 +109,49 @@ fun php(pigeons: Int, holes: Int = pigeons - 1) {
             }
 
             for (j in 1..holes) {
-                println("--- j = $j " + "-".repeat(42))
-                g = bdd.one
-                var timer = 0.0
-                // for (clause in generatePhpClausesBj(pigeons, holes, j)) {
-                //     timer += step("JoinBj") {
-                //         val c = bdd.clause(clause)
-                //         // println("clause $c = $clause")
-                //         g = bdd.applyAnd(g!!, c)
-                //     }
-                // }
-                timer += step("JoinBj") {
-                    g = bdd.cojoinLinear(generatePhpClausesBj(pigeons, holes, j).map { bdd.clause(it) }.asIterable())
-                }
-                // println("JoinBj (j=$j) done in %.3fs".format(timer))
-                timer = 0.0
-                timer += step("JoinB(j=$j)") {
-                    check(!bdd.isZero(f))
-                    f = bdd.applyAnd(f, g!!)
-                }
-                // println("JoinB (j=$j) done in %.3fs".format(timer))
-                g = null
-                if (useProj) {
-                    timer = 0.0
-                    if (j < holes) {
-                        // for (i in 1..pigeons) {
-                        //     timer += step("Proj(${v(i, j)})") {
-                        //         // check(!bdd.isZero(f))
-                        //         f = bdd.exists(f, v(i, j))
-                        //     }
-                        // }
-                        // Note: quantification over the set is better that one-by-one
-                        val qvars = (1..pigeons).map { i -> v(i, j) }.toSet()
-                        timer += step("Proj(j=$j)") {
-                            check(!bdd.isZero(f))
-                            f = bdd.exists(f, qvars)
+                var g = bdd.one
+                if (useCojoinTree) {
+                    step("Cojoin(j=$j)") {
+                        val clausesBj = generatePhpClausesBj(pigeons, holes, j)
+                        g = bdd.cojoinLinear(clausesBj.map { clause -> bdd.clause(clause) }.asIterable())
+                    }
+                } else {
+                    step("Cojoin(j=$j)") {
+                        for (clause in generatePhpClausesBj(pigeons, holes, j)) {
+                            val c = bdd.clause(clause)
+                            g = bdd.applyAnd(g, c)
                         }
                     }
-                    // println("Proj (j=$j) done in %.3fs".format(timer))
+                }
+
+                if (useRelProd) {
+                    step("Join+Proj(j=$j)", useGC) {
+                        check(!bdd.isZero(f))
+                        val qvars = (1..pigeons).map { i -> v(i, j) }.toSet()
+                        f = bdd.relProduct(f, g, qvars)
+                    }
+                } else {
+                    step("JoinB(j=$j)") {
+                        check(!bdd.isZero(f))
+                        f = bdd.applyAnd(f, g)
+                    }
+
+                    if (useProj) {
+                        if (j < holes) {
+                            // for (i in 1..pigeons) {
+                            //     step("Proj(${v(i, j)})") {
+                            //         // check(!bdd.isZero(f))
+                            //         f = bdd.exists(f, v(i, j))
+                            //     }
+                            // }
+                            // Note: quantification over the set is better that one-by-one
+                            val qvars = (1..pigeons).map { i -> v(i, j) }.toSet()
+                            step("Proj(j=$j)") {
+                                check(!bdd.isZero(f))
+                                f = bdd.exists(f, qvars)
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -167,7 +168,6 @@ fun php(pigeons: Int, holes: Int = pigeons - 1) {
     println("bdd.cacheHits = ${bdd.cacheHits}")
     println("bdd.cacheMisses = ${bdd.cacheMisses}")
     println("bdd.maxChain() = ${bdd.maxChain()}")
-    // println("bdd.chains() = ${bdd.chains()}")
     println("-".repeat(42))
 
     val totalTime = PerformanceCounter.reference - timeStart
